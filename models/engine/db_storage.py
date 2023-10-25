deabcbbcd244533ebb20ff48d5ffcdfee587aefb
knapp <emiliodamasco04@gmail.com>
2023-10-25 06:20:43 -0700

    added and fixed methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,111 +9,138 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 
-#//methods of the user
+# Methods of the user
 def sign_up(email, password):
-    """a function to sign up a user"""
+    """Signs up a user"""
     try:
-        user = supabase.auth.sign_up(email=email, password = password)
-    except Exception:
-        raise(Exception)
+        res = supabase.auth.sign_up({
+            'email': email,
+            'password': password,
+        })
+    except:
+        raise Exception()
 
 
-def delete_user(ex_user: str):
-    """A function so that a user can delete themselves"""
+def self_delete(ex_user: str):
+    """Deletes the user's own account"""
     try:
         user = supabase.table("users").delete().eq("username", ex_user)
     except:
-        raise(Exception)
-
-def modify_user():
-    """a function that lets the user modify one of its attributes"""
-
-def invite(user_name: str, new_friend: str):
-    """a function that lets a user send a friend request"""
-    friends = supabase.table("link").insert({"linked_at": datetime.now, "sender_id": user_name})
+        raise Exception()
 
 
-def list_friends(user_name: str)-> List[Dict]:
+def modify_user(user_id: str, column: str, data):
+    """Update values associated with the user"""
+    updt = supabase.table('users').update(
+        {column: data}).eq('id', user_id).execute()
+
+
+def invite(user_id: str, new_friend: str):
+    """Allows a user to send a friend request"""
+    friends = supabase.table("link").insert(
+        {"linked_at": datetime.now, "sender_id": user_id})
+
+
+def list_friends(user_name: str) -> List[Dict]:
     """A function that list the friends of a user"""
     friends = supabase.table("link").select()
 
-def change_status(user_name: str):
-    """a function so that the user can change their status"""
+
+def change_status(user_id: str, status: str):
+    """Change the status of the user"""
+    stat = supabase.table('users').update(
+        {'status': status}).eq('id', user_id).execute()
 
 
-def del_friednf(user: str, exFriend: str):
-    """a function that can allow a user to erase a friend of their friend list"""
+def remove_friend(user: str, exFriend: str):
+    """Removes a Friend from the friends list"""
 
 
 def block_user(user_name: str):
-    """"a function that allows a user to block them so they will not be able to talk any longer"""
+    """Blocks a user"""
 
 
 def report_user(reported: str):
-    """a function that sends a report to another user for misbehavior or some inapropiated behavior"""
+    """Reports a user"""
 
 
-def search_user(user_name: str) -> Dict:
-    """a function that allows the search for a user"""
+def search_user(custom_id: str) -> Dict:
+    """Search a user by their Custom ID"""
     try:
-        user = supabase.table("topics").select(username = user_name)
+        user = supabase.table('users').select('id').eq('custom_id', custom_id).execute()
         return user
     except:
-        raise(Exception)
+        raise Exception()
+
 
 def list_random_chat(user_name: str):
     """a function that lists all your active random chats"""
 
+
 def login(email: str, password: str):
     """a function to verify the identity of the user an allow them to use the page"""
     try:
-        user = supabase.auth.sign_in_with_password({"email":email, "password": password})
+        user = supabase.auth.sign_in_with_password(
+            {"email": email, "password": password})
     except:
-        raise(Exception)
+        raise Exception()
+
+
 def logout():
     """a function so the user can logout"""
     supabase.auth.sign_out()
 
-#// method of the message table
+# // method of the message table
+
+
 def send_message(sender: str, receiver: str, content: str):
     """a function so that an user will be able to send a message to another user"""
 
-#// method of the chat table
+# // method of the chat table
+
+
 def create_chat(user1: str, user2: str):
     """a function to create a chat"""
+
 
 def delete_chat(chat_id):
     """a function that allows to delete a chat when they finish their talk"""
     try:
         chat = supabase.table("chat").delete().eq("chat_id", chat_id)
     except:
-        raise(Exception)
+        raise Exception()
 
-def search_chat(chat_id: str)-> Dict:
+
+def search_chat(chat_id: str) -> Dict:
     """a function that looks for a chat a specific chat"""
     try:
-        chat = supabase.table("chat").select(chat_id = chat_id)
+        chat = supabase.table("chat").select(chat_id=chat_id)
         return chat
     except:
-        raise(Exception)
+        raise Exception()
 
-#// method of the Topic table
-def dictionary_topics()->Dict:
+# // method of the Topic table
+
+
+def dictionary_topics() -> Dict:
     """a function that returns a dictionary with all the topics"""
     try:
         topic = supabase.table("topics").select("*")
         return topic
     except:
-        raise(Exception)
+        raise Exception()
 
-#// method of the Language table
-def list_languages()-> List(str):
+# // method of the Language table
+
+
+def list_languages() -> List(str):
     """returns a list with all the languages supported by speek"""
     try:
         languages = supabase.table("languages").select("*")
         return languages
     except:
-        raise(Exception)
+        raise Exception()
 
-def list_languages(user_name: str)-> List(str):
+
+def list_languages(user_name: str) -> List(str):
     """returns a list with all the languages of the user"""
