fdebd53bb7c124581b81a580389527c57884ca56
Korchea <6334@holbertonstudents.com>
2023-10-31 09:46:29 -0700

    Data to test react created

--- testeos_de_react/src/Data/data_users.js
+++ testeos_de_react/src/Data/data_users.js
@@ -1,72 +1,92 @@
 const data_users = [
     {
         name: "Jotaro Kujo",
-        description: "ORA-ORA-ORA!"
+        description: "ORA-ORA-ORA!",
+        id: 0
     },
     {
         name: "Darth Vader",
-        description: "I am your father"
+        description: "I am your father",
+        id: 1
     },
     {
         name: "Samuel Oak",
-        description: "Are You a Boy? Or Are You a Girl?"
+        description: "Are You a Boy? Or Are You a Girl?",
+        id: 2
     },
     {
         name: "Antonio Stark",
-        description: "I Am 'Iron Man'"
+        description: "I Am 'Iron Man'",
+        id: 3
     },
     {
         name: "Darth Vader",
-        description: "I am your father"
+        description: "I am your father",
+        id: 4
     },
     {
         name: "Samuel Oak",
-        description: "Are You a Boy? Or Are You a Girl?"
+        description: "Are You a Boy? Or Are You a Girl?",
+        id: 5
     },
     {
         name: "Antonio Stark",
-        description: "I Am 'Iron Man'"
-    },    {
+        description: "I Am 'Iron Man'",
+        id: 6
+    },
+    {
         name: "Darth Vader",
-        description: "I am your father"
+        description: "I am your father",
+        id: 7
     },
     {
         name: "Samuel Oak",
-        description: "Are You a Boy? Or Are You a Girl?"
+        description: "Are You a Boy? Or Are You a Girl?",
+        id: 8
     },
     {
         name: "Antonio Stark",
-        description: "I Am 'Iron Man'"
-    },    {
+        description: "I Am 'Iron Man'",
+        id: 9
+    }, {
         name: "Darth Vader",
-        description: "I am your father"
+        description: "I am your father",
+        id: 10
     },
     {
         name: "Samuel Oak",
-        description: "Are You a Boy? Or Are You a Girl?"
+        description: "Are You a Boy? Or Are You a Girl?",
+        id: 11
     },
     {
         name: "Antonio Stark",
-        description: "I Am 'Iron Man'"
+        description: "I Am 'Iron Man'",
+        id: 12
     },
     {
         name: "Samuel Oak",
-        description: "Are You a Boy? Or Are You a Girl?"
+        description: "Are You a Boy? Or Are You a Girl?",
+        id: 13
     },
     {
         name: "Antonio Stark",
-        description: "I Am 'Iron Man'"
-    },    {
+        description: "I Am 'Iron Man'",
+        id: 14
+    },
+    {
         name: "Darth Vader",
-        description: "I am your father"
+        description: "I am your father",
+        id: 15
     },
     {
         name: "Samuel Oak",
-        description: "Are You a Boy? Or Are You a Girl?"
+        description: "Are You a Boy? Or Are You a Girl?",
+        id: 16
     },
     {
         name: "Antonio Stark jejejee",
-        description: "I Am 'Iron Man'"
+        description: "I Am 'Iron Man'",
+        id: 17
     },
 ];
 
