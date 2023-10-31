6ffe017a4b456d744f59ed1550b23116df339417
Mishel450 <tomasmishel18@gmai.com>
2023-10-31 11:06:37 -0700

    .

--- testeos_de_react/src/Components/ChatPage.jsx
+++ testeos_de_react/src/Components/ChatPage.jsx
@@ -4,7 +4,15 @@ export default function Chatpage() {
       backgroundImage: `url(/background-mainpage.png)`,
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center',
-      backgroundSize: 'cover'}}>
+      backgroundSize: 'cover'
+    }}>
+      <div id="chat"></div>
+      <div id="contenedor-de-la-barra">
+        <form id="cont-barra">
+        <input type="text" placeholder="Type a message..." id="barra"></input>
+        <input type="submit" id="enviar-mensaje"></input>
+        </form>
+      </div>
     </div>
   )
 }
