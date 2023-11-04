7d57fca10f0a4db9a072acda463ea80768007ac1
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-11-03 23:14:29 -0700

    friends_initials

--- testeos_de_react/src/Components/Card.jsx
+++ testeos_de_react/src/Components/Card.jsx
@@ -1,13 +1,15 @@
 import "../CSS/Card.css";
 
-export default function Card({ title = "titulo por defecto", last_message = "last message", onCardClick, user_id }) {
+export default function Card({ title = "titulo por defecto", last_message = "last message", friends_initials = "LS", onCardClick, user_id }) {
   const handleCardClick = () => {
     onCardClick(user_id);
   };
 
   return (
     <button className="Card" onClick={handleCardClick}>
-      <div className="perfilLogo"></div>
+      <div className="perfilLogo">
+        <h2>{friends_initials}</h2>
+      </div>
       <div className="userInfo">
         <h3>{title}</h3>
         <h4>{last_message}</h4>
