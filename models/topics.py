57127d585cb4af8605b2ea7cb8d5035154e3fe8e
LeoRod17 <matiassotelo171097@gmail.com>
2023-11-13 06:11:10 -0800

    add random topic

--- models/topics.py
+++ models/topics.py
@@ -21,16 +21,8 @@ def dictionary_topics():
     except:
         raise Exception()
 
-def random_topic(chat_id):
-    """this function it is a random topic"""
-    try:
-        ran = random.randint(1,10)
-        ch = supabase.table('chats').update({'topic_id': ran}).eq('chat_id', chat_id).execute()
-        return ch
-    except:
-        raise Exception()
-
-def search_topic(topic_id):
+def search_topic():
     """a function that returns the topic"""
-    topic = supabase.table('topics').select('topic').eq('topic', topic_id).execute()
+    ran = random.randint(1,10)
+    topic = supabase.table('topics').select('topic').eq('id', ran).execute()
     return topic
