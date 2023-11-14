44d43ba5b6ee18c65b6686bdfe23adbe707bf12c
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-11-14 08:11:54 -0800

    Send Friends button

--- testeos_de_react/src/Components/ChatPage.jsx
+++ testeos_de_react/src/Components/ChatPage.jsx
@@ -53,8 +53,20 @@ export default function Chatpage({ user_id, mostrarCaja, personal_id }) {
     }}>
       {mostrarCaja && <div id="perfilebox">
         <div className="topPerfilebox">
-          <p> {user_id} </p>
+          <p> User Name </p>
         </div>
+        <div className="descriptionBox1">
+          <p>Bio</p>
+        </div>
+        <div className="descriptionBox2">
+          <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.</h4>
+        </div>
+        <div className="descriptionBox3">
+          <p>Lenguages</p>
+        </div>
+        <button className="friendRequestButton">
+          <h3>Send friend request</h3>
+        </button>
 
       </div>}
       <div id="cont-chat">
